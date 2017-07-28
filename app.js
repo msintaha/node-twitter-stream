5a6beb205c6623c83818c5ee9c6dd426dec1cd5b
msintaha <msintaha94@gmail.com>
2017-07-28 10:12:06 +0600

    Upgrade node version and use const

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express'),
+const express = require('express'),
     app     = express(),
     port    = process.env.PORT || 3000,
     hbs  = require('express-handlebars'),
@@ -15,18 +15,18 @@ app.get('/', function(req, res) {
 
 server.listen(port);
 
-var twitter = new Twit({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+const twitter = new Twit({
+    consumer_key: 'ClkspMmRMOTAJvysssMpylY56',
+    consumer_secret: '34qxajeFKcPTvMocYT5xMdOTZrQohbOAO1btgpRihJuA1tGxlM',
+    access_token: '138086873-jMTpMLwOTPKRRDQ9MnlgUvwIollZTadG3i9ZnRlz',
+    access_token_secret: 'xFdxY1bV2aRaxvy5AJ16i7kCxsVNe6MzThTr5QgJxJzwJ'
 });
 
-var stream = twitter.stream('statuses/filter', { track: 'javascript,angular' });
+const stream = twitter.stream('statuses/filter', { track: 'javascript,angular' });
 
 io.on('connect', function(socket) {
     stream.on('tweet', function (tweet) {
-        var data = {
+        const data = {
             'name': tweet.user.name,
             'screen_name': tweet.user.screen_name,
             'text': tweet.text,
